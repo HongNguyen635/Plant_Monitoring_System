74039033b971de655734fc8ab597385d215dc014
Donald Philips <donaldwphilips@gmail.com>
2024-11-23 15:04:29 -0800

    bluetooth communication code, missing moisture sensor

--- Microcontrollers_Code/Mega_Central/Mega_Central.ino
+++ Microcontrollers_Code/Mega_Central/Mega_Central.ino
@@ -2,6 +2,13 @@
 
 #include <SoftwareSerial.h>
 
+#define PHOTOCELL_CHAR 'A'
+#define TEMP_CHAR 'B'
+#define MOISTURE_CHAR 'C'
+#define TEMP_SOIL_CHAR 'D'
+#define IR_SENSOR1 'E'
+#define IR_SENSOR2 'F'
+
 // setup bluetooth
 SoftwareSerial BTSerial(12, 2); // RX, TX
 
@@ -9,10 +16,75 @@ SoftwareSerial BTSerial(12, 2); // RX, TX
 void setup() 
 {
   BTSerial.begin(38400);
+  Serial.begin(9600);
 }
 
 // test communication to uno
 void loop()
 {
- BTSerial.write('1');
+  // BTSerial.write('B');
+  // while (!BTSerial.available()) {}
+  // char BT = BTSerial.read();
+  // if (BT == 'PHOTOCELL_CHAR') {
+  //   Serial.println("received data");
+  // } else {
+  //   Serial.println("incorrect data received");
+  // }
+  // delay(100);
+
+  // int photocell = requestPhotocell();
+  // Serial.println(photocell);
+
+  int Temp = requestTemp();
+  Serial.println(Temp);
+
+  // bool IR1 = requestIR1();
+  // if (IR1) {
+  //   Serial.println("object present");
+  // }
+
+  // bool IR2 = requestIR2();
+  // if (IR2) {
+  //   Serial.println("object present");
+  // }
+}
+
+int requestPhotocell() {
+  BTSerial.write(PHOTOCELL_CHAR);
+  while (!BTSerial.available()) {}
+  uint8_t hi = BTSerial.read();
+  BTSerial.write('0');
+  while (!BTSerial.available()) {}
+  uint8_t lo = BTSerial.read();
+  uint16_t BT = (hi << 8) + lo;
+  return BT;
+}
+
+bool requestIR1() {
+  BTSerial.write(IR_SENSOR1);
+  while (!BTSerial.available()) {}
+  char BT = BTSerial.read();
+  return !(BT == 'H');
+}
+
+bool requestIR2() {
+  BTSerial.write(IR_SENSOR2);
+  while (!BTSerial.available()) {}
+  char BT = BTSerial.read();
+  return !(BT == 'H');
+}
+
+int requestTemp() {
+  BTSerial.write(TEMP_CHAR);
+  while (!BTSerial.available()) {}
+  int BT = BTSerial.read();
+  return BT;
+}
+
+int requestTempSoil() {
+
+}
+
+int requestMoisture() {
+
 }
--- Microcontrollers_Code/Uno_Peripheral/Uno_Peripheral.ino
+++ Microcontrollers_Code/Uno_Peripheral/Uno_Peripheral.ino
@@ -2,6 +2,13 @@
 
 #include <SoftwareSerial.h>
 
+#define PHOTOCELL_CHAR 'A'
+#define TEMP_CHAR 'B'
+#define MOISTURE_CHAR 'C'
+#define TEMP_SOIL_CHAR 'D'
+#define IR_SENSOR1 'E'
+#define IR_SENSOR2 'F'
+
 SoftwareSerial BTSerial(10, 11); // RX, TX
 
 // setup serial monitor and bluetooth
@@ -12,12 +19,47 @@ void setup()
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
 }
 
 // test bluetooth to read msg from mega
 void loop() 
 {
   if (BTSerial.available()) {
-    Serial.println(BTSerial.read() - '0');
+    char BT = BTSerial.read();
+    if (BT == PHOTOCELL_CHAR) {
+      Serial.println(analogRead(A0));
+      uint16_t photocell = analogRead(A0);
+      Serial.println(photocell);
+      uint8_t hi = photocell >> 8;
+      uint8_t lo = photocell & 0xFF;
+      BTSerial.write(hi);
+      while (!BTSerial.available()) {}
+      BTSerial.write(lo);
+    } else if (BT == TEMP_CHAR) {
+      BTSerial.write(analogRead(A1));
+      Serial.println(analogRead(A1));
+    } else if (BT == MOISTURE_CHAR) {
+      BTSerial.write(MOISTURE_CHAR);
+    } else if (BT == TEMP_SOIL_CHAR) {
+      BTSerial.write(TEMP_SOIL_CHAR);
+    } else if (BT == IR_SENSOR1) {
+      if (digitalRead(2) == HIGH) {
+        BTSerial.write('H');
+      } else {
+        BTSerial.write('L');
+      }
+    } else if (BT == IR_SENSOR2) {
+      if (digitalRead(3) == HIGH) {
+        BTSerial.write('H');
+      } else {
+        BTSerial.write('L');
+      }
+    } else {
+      Serial.println("char not recognized");
+    }
   }
 }
