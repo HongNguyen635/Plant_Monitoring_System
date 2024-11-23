08e61a0ae263304ca778836485ee43c25d82ab9b
Hong635 <hongkng5@uw.edu>
2024-11-23 15:25:19 -0800

    update bluetooth

--- Microcontrollers_Code/Mega_Central/Mega_Central.ino
+++ Microcontrollers_Code/Mega_Central/Mega_Central.ino
@@ -1,18 +1,112 @@
 //this is master
 
 #include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
+// components pins
+#define displaySDA 20
+#define displaySCL 21
+
+// bluetooth assigned char
+#define PHOTOCELL_CHAR 'A'
+#define TEMP_CHAR 'B'
+#define MOISTURE_CHAR 'C'
+#define TEMP_SOIL_CHAR 'D'
+#define IR_SENSOR1 'E'
+#define IR_SENSOR2 'F'
 
 // setup bluetooth
 SoftwareSerial BTSerial(12, 2); // RX, TX
+
+// set up display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // set up the baud rate for bluetooth
 void setup() 
 {
   BTSerial.begin(38400);
+
+  lcd.backlight();
+  lcd.init(); 
 }
 
 // test communication to uno
 void loop()
 {
- BTSerial.write('1');
+  // BTSerial.write('1');
+
+  // test display
+  // lcd.clear();
+  // lcd.setCursor(0, 0); 
+  // lcd.print("Test message");
+
+
+
+
+  // BTSerial.write('B');
+  // while (!BTSerial.available()) {}
+  // char BT = BTSerial.read();
+  // if (BT == 'PHOTOCELL_CHAR') {
+  //   Serial.println("received data");
+  // } else {
+  //   Serial.println("incorrect data received");
+  // }
+  // delay(100);
+
+  // int photocell = requestPhotocell();
+  // Serial.println(photocell);
+
+  int Temp = requestTemp();
+  Serial.println(Temp);
+
+  // bool IR1 = requestIR1();
+  // if (IR1) {
+  //   Serial.println("object present");
+  // }
+
+  // bool IR2 = requestIR2();
+  // if (IR2) {
+  //   Serial.println("object present");
+  // }
+}
+
+
+int requestPhotocell() {
+  BTSerial.write(PHOTOCELL_CHAR);
+  while (!BTSerial.available()) {}
+  uint8_t hi = BTSerial.read();
+  BTSerial.write('0');
+  while (!BTSerial.available()) {}
+  uint8_t lo = BTSerial.read();
+  uint16_t BT = (hi << 8) + lo;
+  return BT;
+}
+
+bool requestIR1() {
+  BTSerial.write(IR_SENSOR1);
+  while (!BTSerial.available()) {}
+  char BT = BTSerial.read();
+  return !(BT == 'H');
+}
+
+bool requestIR2() {
+  BTSerial.write(IR_SENSOR2);
+  while (!BTSerial.available()) {}
+  char BT = BTSerial.read();
+  return !(BT == 'H');
+}
+
+int requestTemp() {
+  BTSerial.write(TEMP_CHAR);
+  while (!BTSerial.available()) {}
+  int BT = BTSerial.read();
+  return BT;
+}
+
+int requestTempSoil() {
+
+}
+
+int requestMoisture() {
+
 }
--- Microcontrollers_Code/Uno_Peripheral/Uno_Peripheral.ino
+++ Microcontrollers_Code/Uno_Peripheral/Uno_Peripheral.ino
@@ -2,6 +2,25 @@
 
 #include <SoftwareSerial.h>
 
+// sensors pins
+#define photocellPin A0
+#define temperaturePin A1
+#define irSensor1 2
+#define irSensor2 3
+#define buzzerPin 5
+
+// bluetooth communication char
+#define PHOTOCELL_CHAR 'A'
+#define TEMP_CHAR 'B'
+#define MOISTURE_CHAR 'C'
+#define TEMP_SOIL_CHAR 'D'
+#define IR_SENSOR1 'E'
+#define IR_SENSOR2 'F'
+
+volatile bool objectPresentSensor1 = false;
+bool objectPresentSensor2 = false;
+
+
 SoftwareSerial BTSerial(10, 11); // RX, TX
 
 // setup serial monitor and bluetooth
@@ -10,14 +29,170 @@ void setup()
 {
   BTSerial.begin(38400);
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+
+
+  // INPUT:
+  //pinMode(irSensor1, INPUT); 
+  pinMode(irSensor1, INPUT_PULLUP); 
+  pinMode(irSensor2, INPUT); 
+
+
+  // OUTPUT
+  pinMode(buzzerPin, OUTPUT);
+
+  // IR sensor
+  attachInterrupt(digitalPinToInterrupt(irSensor1), irSensor1_ISR, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(irSensor2), irSensor2_ISR, FALLING); 
+
 }
 
 // test bluetooth to read msg from mega
 void loop() 
 {
+  // TODO: uncomment later
+  // if (BTSerial.available()) {
+  //   Serial.println(BTSerial.read() - '0');
+  // }
+
+
+  if (objectPresentSensor1) {
+    if (digitalRead(irSensor1) == LOW) {
+      Serial.println("object");
+
+          // buzzer
+          tone(buzzerPin, 100);
+          delay(1000);
+          noTone(buzzerPin); 
+    }
+    else {
+        Serial.println("false alarm");
+    }
+    
+          objectPresentSensor1 = false;
+    // reattch to interrupt to detect object after beeps
+    //attachInterrupt(digitalPinToInterrupt(irSensor1), irSensor1_ISR, FALLING); 
+  }
+  else {
+    Serial.println("no object");
+  }
+
+  // if (objectPresentSensor2) {
+  // Serial.println("object");
+  // objectPresentSensor2 = false;
+
+  // // buzzer
+  // tone(buzzerPin, 100);
+  // delay(1000);
+  // noTone(buzzerPin); 
+
+
+  // // reattch to interrupt to detect object after beeps
+  // attachInterrupt(digitalPinToInterrupt(irSensor2), irSensor2_ISR, FALLING); 
+  // }
+
+  //Serial.println(digitalRead(irSensor1));
+
+  // ------------ TODO: FIX ABOVE CODE LATER ----------
+
+
+  // ---------- Bluetooth Reading ------------
   if (BTSerial.available()) {
-    Serial.println(BTSerial.read() - '0');
+    char BT = BTSerial.read();
+    if (BT == PHOTOCELL_CHAR) {
+      Serial.println(analogRead(photocellPin));
+      uint16_t photocell = analogRead(photocellPin);
+      Serial.println(photocell);
+      uint8_t hi = photocell >> 8;
+      uint8_t lo = photocell & 0xFF;
+      BTSerial.write(hi);
+      while (!BTSerial.available()) {}
+      BTSerial.write(lo);
+    } else if (BT == TEMP_CHAR) {
+      BTSerial.write(analogRead(temperaturePin));
+      Serial.println(analogRead(temperaturePin));
+    } else if (BT == MOISTURE_CHAR) {
+      BTSerial.write(MOISTURE_CHAR);
+    } else if (BT == TEMP_SOIL_CHAR) {
+      BTSerial.write(TEMP_SOIL_CHAR);
+    } else if (BT == IR_SENSOR1) {
+      if (digitalRead(irSensor1) == HIGH) {
+        BTSerial.write('H');
+      } else {
+        BTSerial.write('L');
+      }
+    } else if (BT == IR_SENSOR2) {
+      if (digitalRead(irSensor2) == HIGH) {
+        BTSerial.write('H');
+      } else {
+        BTSerial.write('L');
+      }
+    } else {
+      Serial.println("char not recognized");
+    }
   }
 }
+
+
+// --------------------------------------
+// SENSOR READING METHODS
+// --------------------------------------
+
+// get the photocell reading
+String getPhototocellSensor() {
+  // get the raw photocell reading
+  int photocellReading = analogRead(photocellPin);  
+  Serial.println(photocellReading);
+ 
+  // We'll have a few threshholds, qualitatively determined
+  if (photocellReading < 100) {
+    return "Dark";
+
+  } else if (photocellReading < 300) {
+    return "Dim";
+
+  } else if (photocellReading < 500) {
+    return "Light";
+
+  } else if (photocellReading < 900) {
+    return "Bright";
+
+  } else {
+    return "Very bright!";
+  }
+}
+
+// get temperature reading
+float getTemperatureSensor() {
+  int reading = analogRead(temperaturePin);  
+ 
+  // converting that reading to voltage
+  float voltage = reading * 5.0;
+  voltage /= 1024.0; 
+  
+  // now print out the temperature
+  float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                                //to degrees ((voltage - 500mV) times 100)
+  
+  // convert to Fahrenheit
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+
+  return temperatureF;
+}
+
+
+// IR sensors ISR
+void irSensor1_ISR() {
+  //detachInterrupt(digitalPinToInterrupt(irSensor1_ISR)); 
+  objectPresentSensor1 = true;
+}
+
+void irSensor2_ISR() {
+  detachInterrupt(digitalPinToInterrupt(irSensor2_ISR)); 
+  objectPresentSensor2 = true;
+}
+
+
+// --------------------------------------
+// UTILITY METHODS
+// --------------------------------------
+
